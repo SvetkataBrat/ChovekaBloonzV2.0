d28466f22f1181699dddd183c5434cbf3d813d28
Hristp Atanasov <itcaka50@abv.bg>
2023-05-06 11:53:48 +0300

    layeri

---
+++ TestingLayer/SaloniContextTest.cs
@@ -0,0 +1,124 @@
+﻿using BusinessLayer;
+using DataLayer;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingLayer
+{
+    [TestFixture]
+    public class SaloniContextTest
+    {
+        private SaloniContext context = new(SetupFixture.dbContext);
+        private Saloni saloni;
+        private Auto auto1;
+        private Auto auto2;
+
+        [SetUp]
+        public void Setup()
+        {
+            saloni = new(1112, "Coleto", "coleto st.");
+
+            auto1 = new(1111, "BMW", 2, 4, 1112, 1);
+            auto2 = new(1113, "colajajeep", 100000, 99999, 1112, 1);
+
+            saloni.Cars.Add(auto1);
+            saloni.Cars.Add(auto2);
+
+            context.Create(saloni);
+        }
+
+        [TearDown]
+        public void DropSaloni()
+        {
+            foreach (Saloni item in SetupFixture.dbContext.Salons.ToList())
+            {
+                SetupFixture.dbContext.Salons.Remove(item);
+            }
+
+            SetupFixture.dbContext.SaveChanges();
+        }
+
+
+        [Test]
+        public void Create()
+        {
+            Saloni testSalon = new(1115, "cc", "kotkist");
+
+            int salonsBefore = SetupFixture.dbContext.Salons.Count();
+
+            context.Create(testSalon);
+
+            int salonsAfter = SetupFixture.dbContext.Salons.Count();
+
+            Assert.That(salonsBefore + 1 == salonsAfter, "Create() does not work!");
+        }
+
+        [Test]
+        public void Read()
+        {
+            Saloni readSalon = context.Read(saloni.Id);
+
+            Assert.AreEqual(saloni, readSalon, "Read does not return the same object!");
+        }
+
+        [Test]
+        public void ReadWithNavigationalProperties()
+        {
+            Saloni readSalon = context.Read(saloni.Id);
+            Assert.That(readSalon.Cars.Contains(auto1)
+                && readSalon.Cars.Contains(auto2),
+                "auto1 and auto2 are not in the Cars list!");
+        }
+
+        [Test]
+        public void ReadAll()
+        {
+            List<Saloni> salonite = (List<Saloni>)context.ReadAll();
+
+            Assert.That(salonite.Count != 0, "ReadAll() does not return cupboards!");
+        }
+
+        [Test]
+        public void ReadAllWithNavigationalProperties()
+        {
+            Saloni readSalon = new(1111, "colaja", "barona st.");
+            Auto auto = new(1, "Ferrari", 12, 14, readSalon.Id, 1);
+            SetupFixture.dbContext.Autos.Add(auto);
+            SetupFixture.dbContext.Salons.Add(readSalon);
+            context.Create(readSalon);
+
+            List<Saloni> salons = (List<Saloni>)context.ReadAll();
+
+            Assert.That(salons.Count != 0
+                && context.Read(readSalon.Id).Cars.Count == 1, "ReadAll() does not return cupboards!");
+        }
+
+        [Test]
+        public void Update()
+        {
+            Saloni changedSalon = context.Read(saloni.Id);
+
+            changedSalon.Name = "COLAJASHOP";
+            changedSalon.Adress = "peshtersko shose";
+
+            context.Update(changedSalon);
+
+            Assert.AreEqual(changedSalon, saloni, "Update() does not work!");
+        }
+
+        [Test]
+        public void Delete()
+        {
+            int salonsBefore = SetupFixture.dbContext.Salons.Count();
+
+            context.Delete(saloni.Id);
+            int salonsAfter = SetupFixture.dbContext.Salons.Count();
+
+            Assert.IsTrue(salonsBefore - 1 == salonsAfter, "Delete() does not work!");
+        }
+    }
+}
