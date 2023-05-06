b2d81fad741d732837bfe6e2f5937f6c040b242c
Hristp Atanasov <itcaka50@abv.bg>
2023-05-06 09:07:23 +0300

    class Saloni

---
+++ BusinessLayer/Saloni.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    public class Saloni
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Adress { get; set; }
+
+        public List<Auto> Cars { get; set; }
+
+        private Saloni()
+        {
+            Cars = new List<Auto>();
+        }
+
+        public Saloni(int id, string name, string adress)
+        {
+            Id = id;
+            Name = name;
+            Adress = adress;
+        }
+    }
+}
