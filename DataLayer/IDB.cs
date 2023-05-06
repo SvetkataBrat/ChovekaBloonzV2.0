0ae5d01d747d2f5a1aaa02d9a6700c1947f572e7
monski2005 <monski2005@abv.bg>
2023-05-06 09:53:26 +0300

    added IDB

---
+++ DataLayer/IDB.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    public interface IDB<T, K> where K : IConvertible
+    {
+        void Create(T item);
+
+        T Read(K key);
+
+        IEnumerable<T> ReadAll();
+
+        void Update(T item);
+
+        void Delete(K key);
+
+    }
+}
