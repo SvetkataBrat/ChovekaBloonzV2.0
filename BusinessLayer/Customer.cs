e405b5faefa8dcf7f6a6a53f6e7219c8b3e5b04e
monski2005 <monski2005@abv.bg>
2023-05-06 09:08:52 +0300

    added Customer and DataLayer

---
+++ BusinessLayer/Customer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    public class Customer
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required] 
+        public string FirstName { get; set; }
+
+        [Required] 
+        public string LastName { get; set; }
+
+        public List<Auto> cars { get; set; }
+
+        private Customer()
+        {
+            this.cars = new List<Auto>();   
+        }
+
+        public Customer(string fName, string lName)
+        {
+            this.cars = new List<Auto>();
+            this.FirstName = fName;
+            this.LastName = lName;
+        }
+    }
+}
