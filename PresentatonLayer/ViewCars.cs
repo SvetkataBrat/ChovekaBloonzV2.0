a8f756476fabc710acdf5bdddbdff45dd14eaf2e
Galabin Stamatov <lachko.mitev@abv.bg>
2023-06-11 18:31:48 +0300

    view car

--- PresentatonLayer/ViewCars.cs
+++ PresentatonLayer/ViewCars.cs
@@ -1,20 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using BusinessLayer;
+using ServiceLayer;
 
 namespace PresentatonLayer
 {
     public partial class ViewCars : Form
     {
+        Auto selectedAuto;
+        Saloni selectedSalon;
+
+        DbManager<Auto, int> autoManager = new DbManager<Auto, int>(ContextGenerator.GetAutoContext());
+        DbManager<Saloni, int> salonManager = new DbManager<Saloni, int>(ContextGenerator.GetSaloniContext());
+
+
         public ViewCars()
         {
             InitializeComponent();
         }
+
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        
     }
 }
