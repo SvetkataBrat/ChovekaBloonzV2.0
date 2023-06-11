c6288d23195b455a168e8cf599331a7b9eabf21f
Galabin Stamatov <lachko.mitev@abv.bg>
2023-06-11 17:05:02 +0300

    aaa

---
+++ PresentationLayer/Form1.cs
@@ -0,0 +1,20 @@
+namespace PresentationLayer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "baronaja";
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
